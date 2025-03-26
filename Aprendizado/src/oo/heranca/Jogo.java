c0fd714827ac5cec5698549e26560afc24a25747
Matheus-Mastellona <matheusmastelloni@gmail.com>
2025-03-26 11:28:27 -0300

    Herança #02

--- Aprendizado/src/oo/heranca/Jogo.java
+++ Aprendizado/src/oo/heranca/Jogo.java
@@ -2,28 +2,30 @@ package oo.heranca;
 
 public class Jogo {
     public static void main(String[] args) {
-        Monstro j1 = new Monstro();
-        j1.x = 10;
-        j1.y = 10;
+        Monstro monstro = new Monstro();
+        monstro.x = 10;
+        monstro.y = 10;
 
-        Heroi j2 = new Heroi();
-        j2.x = 10;
-        j2.y = 11;
+        Heroi heroi = new Heroi();
+        heroi.x = 10;
+        heroi.y = 11;
 
-        System.out.println(j1.vida);
-        System.out.println(j2.vida);
+        System.out.println("O Monstro tem => " + monstro.vida + " HP");
+        System.out.println("O Heroi tem => " + heroi.vida + " HP");
 
-        j1.atacar(j2);
-        j2.atacar(j1);
+        monstro.atacar(heroi);
+        heroi.atacar(monstro);
 
-        System.out.println(j1.vida);
-        System.out.println(j2.vida);
+        System.out.println("O Monstro tem => " + monstro.vida + " HP");
+        System.out.println("O Heroi tem => " + heroi.vida + " HP");
 
-        j2.andar(Direcao.SUL);
-        j1.atacar(j2);
-        System.out.println(j2.vida);
 
         /*
+         * heroi.andar(Direcao.SUL);
+         * monstro.atacar(heroi);
+         * System.out.println(heroi.vida);
+         * 
+         * 
          * j1.andar(Direcao.NORTE);
          * j1.andar(Direcao.LESTE);
          * j1.andar(Direcao.NORTE);
