dbd310b998a7621b3196d82896fca221926c9a41
Matheus-Mastellona <matheusmastelloni@gmail.com>
2025-03-28 14:16:34 -0300

    Interface #2

--- Aprendizado/src/oo/heranca/desafio/Luxo.java
+++ Aprendizado/src/oo/heranca/desafio/Luxo.java
@@ -1,6 +1,9 @@
 package oo.heranca.desafio;
 
-public interface Luxo {
+public interface Luxo { // public ou abstratc cabe nas vairáveis
     void ligarAr();
     void desligaAr();
+    default int velocidadeDoAr() { // default n obriga a realização do metodo
+        return 1;
+    }
 }
