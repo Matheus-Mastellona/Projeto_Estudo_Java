0ce9f0c625f8a061fe700e4b2711a79844bc24b5
Matheus-Mastellona <matheusmastelloni@gmail.com>
2025-03-28 15:36:34 -0300

    Interfaces Funcionais #01
    Aparentemente a lambda só funciona se houver apenas um metodo na classe, fora isso, ela não irá funcionar.

--- Aprendizado/src/lambda/Calculo.java
+++ Aprendizado/src/lambda/Calculo.java
@@ -1,7 +1,8 @@
 package lambda;
 
+@FunctionalInterface // faz com que a interface aceite apenas um metodo, vantagens no quesito da lambida
 public interface Calculo {
 
     public abstract double execeutar(double a, double b);
-    
+
 }
