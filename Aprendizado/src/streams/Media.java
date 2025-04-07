4339849ee0e1c184f1dd4edca74ecbe1087f7993
Matheus Mastellona <matheusmastelloni@gmail.com>
2025-04-06 23:35:23 -0300

    Updates

---
+++ Aprendizado/src/streams/Media.java
@@ -0,0 +1,15 @@
+package streams;
+
+public class Media {
+    private double total;
+    private int quantidade;
+
+    public void adicionar(double valor) {
+        total += valor;
+        quantidade++;
+    }
+
+    public double getValor() {
+        return total / quantidade;
+    }
+}
