42e134c21a5399f26c4a2f33a6f042480a9183a2
Matheus-Mastellona <matheusmastelloni@gmail.com>
2025-04-10 13:50:57 -0300

    Classe Campo: Adicionar Vizinhos

--- campo-minado/src/br/com/mastelloni/cm/modelo/Campo.java
+++ campo-minado/src/br/com/mastelloni/cm/modelo/Campo.java
@@ -19,6 +19,21 @@ public class Campo {
     }
 
     boolean adicionarVizinho(Campo vizinho) {
+        boolean linhaDiferete = linha != vizinho.linha;
+        boolean colunaDiferete = coluna != vizinho.coluna;
+        boolean diagonal = linhaDiferete && colunaDiferete;
+
+        int deltaLinha = Math.abs(linha - vizinho.linha);
+        int deltaColuna = Math.abs(coluna - vizinho.coluna);
+        int deltaGeral = deltaColuna + deltaLinha;
+
+        if (deltaGeral == 1 && !diagonal) {
+            vizinhos.add(vizinho);
+            return true;
+        } else if (deltaGeral == 2 && diagonal) {
+            vizinhos.add(vizinho);
+            return true;
+        }
         return false;
     }
 }
