a2bc3f4aa2167812371c63f6fd4b12bb02b0a441
Matheus Mastellona <matheusmastelloni@gmail.com>
2025-04-11 11:41:03 -0300

    Básico COM Generics 2/2

--- Aprendizado/src/generics/CaixaTeste.java
+++ Aprendizado/src/generics/CaixaTeste.java
@@ -4,7 +4,17 @@ public class CaixaTeste {
     public static void main(String[] args) {
         Caixa<String> caixaA = new Caixa<>();
         caixaA.aguardar("Segredo!");
+
         String coisaA = caixaA.abrir();
         System.out.println(coisaA);
+
+        Caixa<Double> caixaB = new Caixa<>();
+        caixaB.aguardar(3.1415);
+
+        Double coisaB = caixaB.abrir();
+        System.out.println(coisaB);
+
+        // String coisaA = caixaA.abrir();
+        // System.out.println(coisaA);
     }
 }
