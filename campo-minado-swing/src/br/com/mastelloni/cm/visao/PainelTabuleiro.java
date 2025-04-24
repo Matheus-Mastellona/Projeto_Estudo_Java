ba3156dbc192f17c46f3d430d855ac2176642ec0
Matheus-Mastellona <matheusmastelloni@gmail.com>
2025-04-24 13:57:43 -0300

    Interface Gráfica do Campo Minado 1/6

---
+++ campo-minado-swing/src/br/com/mastelloni/cm/visao/PainelTabuleiro.java
@@ -0,0 +1,12 @@
+package br.com.mastelloni.cm.visao;
+
+import javax.swing.JPanel;
+
+import br.com.mastelloni.cm.modelo.Tabuleiro;
+
+public class PainelTabuleiro extends JPanel {
+
+    public PainelTabuleiro(Tabuleiro tabuleiro) {
+
+    }
+}
