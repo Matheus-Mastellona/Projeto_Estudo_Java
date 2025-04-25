5becfc7b9d65c828260a342b83b517c6ca463482
Matheus-Mastellona <matheusmastelloni@gmail.com>
2025-04-25 11:07:55 -0300

    Lógica da Calculadora 2/6

--- calculadora/src/br/com/mastelloni/calc/modelo/Memoria.java
+++ calculadora/src/br/com/mastelloni/calc/modelo/Memoria.java
@@ -1,8 +1,13 @@
 package br.com.mastelloni.calc.modelo;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Memoria {
 
     private static final Memoria instancia = new Memoria();
+
+    private final List<MemoriaObservador> observadores = new ArrayList<>();
 
     private String textoAutal = "";
 
@@ -14,7 +19,21 @@ public class Memoria {
         return instancia;
     }
 
+    public void adicionarObservador(MemoriaObservador observador) {
+        observadores.add(observador);
+    }
+
     public String getTextoAtual() {
         return textoAutal.isEmpty() ? "0" : textoAutal;
+    }
+
+    public void processarComando(String valor) {
+
+        if ("AC".equals(valor)) {
+            textoAutal = "";
+        } else {
+            textoAutal += valor;
+        }
+        observadores.forEach(o -> o.valorAlterado(getTextoAtual()));
     }
 }
--- calculadora/src/br/com/mastelloni/calc/visao/Teclado.java
+++ calculadora/src/br/com/mastelloni/calc/visao/Teclado.java
@@ -9,6 +9,8 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
+import br.com.mastelloni.calc.modelo.Memoria;
+
 public class Teclado extends JPanel implements ActionListener { // JPanel é um container
 
     private final Color COR_CINZA_ESCURO = new Color(68, 68, 68);
@@ -73,7 +75,7 @@ public class Teclado extends JPanel implements ActionListener { // JPanel é um 
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() instanceof JButton) {
             JButton botao = (JButton) e.getSource();
-            System.out.println(botao.getText());
+            Memoria.getInstancia().processarComando(botao.getText());
         }
     }
 }
